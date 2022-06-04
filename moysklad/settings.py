ae77135016c65f2c0e900c032b2d2853738d3f9f
Syimyk_Jakshybaev <74294748+SyimykZhak@users.noreply.github.com>
2022-06-04 10:54:41 +0300

    Update settings.py

--- moysklad/settings.py
+++ moysklad/settings.py
@@ -16,16 +16,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k%7a0-)$6@4x(0#ajzf3zf%bbfg5kr53t(fx5^(zqy-k@-0-e4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
